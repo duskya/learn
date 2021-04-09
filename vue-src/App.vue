39710e5c33f388f19deea3e33280008d8146da7f
阿巴阿巴阿巴 <76472457+duskya@users.noreply.github.com>
2021-04-09 23:21:26 +0800

    Add files via upload

---
+++ vue-src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- <List /> -->
+  <todoList />
+</template>
+<script>
+// import List from './components/list'
+import todoList from './components/todoList'
+
+export default {
+  name: 'App',
+  components: {
+    // List,
+    todoList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-src/components/list.vue
@@ -0,0 +1,55 @@
+// def
+<template>
+<div>
+  <h1>{{count}} *2={{double}}</h1>  
+  <h2>{{num}}</h2>
+  <!-- <h3>{{onMounted}}</h3> -->
+  <button @click="add">累加</button>
+</div>
+</template>
+
+<script>
+import {computed,onMounted,reactive,ref, toRefs} from 'vue'
+//computed 对象
+export default {
+    
+    setup(){
+        const {state , double} =useCounter(1)
+        //入口函数
+        // const state = reactive({//响应式方法
+        //     count:1
+        // })
+        const num = ref(2)// ref原始类型变成响应式
+    
+        function add(){
+            state.count++
+            console.log(state.count)
+        }
+        // const double = computed(() =>state.count*2)
+
+        // onMounted(() =>{
+        //     console.log('onMounted');
+        // })
+
+        return {
+            ...toRefs(state),
+            // state,
+            add,
+            double,
+            num,
+            onMounted
+        }
+    }
+}
+function useCounter(count){
+    const state = reactive({
+        count
+    })
+    const double = computed(() =>state.count *2)
+    return{state,double}
+}
+</script>
+
+<style>
+
+</style>
